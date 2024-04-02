4ef4e48164c4cc6425eb659b79973eac6dd113bb
unknown <axtra2@gmail.com>
2024-04-02 16:13:06 +0300

    fmap error handling

--- src/stream/fmap.go
+++ src/stream/fmap.go
@@ -1,14 +1,40 @@
 package stream
 
-import "reflect"
+import (
+	"fmt"
+	"reflect"
+)
 
 func (s Stream) Fmap(f interface{}) Stream {
-	// TODO: add param type check
-	// TODO: add return type check (val or (val, err))
+	// TODO: replace with s.Valid()
+	if s.err != nil {
+		return s
+	}
+	fType := reflect.TypeOf(f)
+	// return type check
+	if !(fType.NumOut() == 1 || (fType.NumOut() == 2 && fType.Out(1).Implements(reflect.TypeOf((*error)(nil)).Elem()))) {
+		// TODO: better error message with actual types
+		return s.Error(fmt.Errorf("function used in fmap must return value or (value, error)"))
+	}
+
+	// param type check
+	if fType.NumIn() != 1 {
+		// TODO: better error message with actual types
+		return s.Error(fmt.Errorf("function used in fmap must recieve 1 argument"))
+	}
+	if fType.In(0) != s.elemType {
+		// TODO: better error message with actual types
+		return s.Error(fmt.Errorf("type of argument 1 in function must be the same as element type of a stream"))
+	}
+
 	res := reflect.MakeSlice(reflect.SliceOf(reflect.TypeOf(f).Out(0)), 0, s.len)
 	for i := 0; i < s.len; i++ {
 		args := append([]reflect.Value{}, s.value.Index(i))
-		res = reflect.Append(res, reflect.ValueOf(f).Call(args)[0])
+		newVals := reflect.ValueOf(f).Call(args)
+		if len(newVals) == 2 && !newVals[1].IsNil() {
+			return s.Error(newVals[1].Interface().(error))
+		}
+		res = reflect.Append(res, newVals[0])
 	}
 	s.elemType = res.Type().Elem()
 	s.value = res
