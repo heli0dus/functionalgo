430b2294c28bd570ef2bd1e5c0ffc8d1230fd880
unknown <axtra2@gmail.com>
2024-04-02 18:57:48 +0300

    proof of concept example

--- src/main.go
+++ src/main.go
@@ -2,107 +2,43 @@ package main
 
 import (
 	"fmt"
+	"os"
+	"strconv"
 
 	"github.com/heli0dus/functionalgo/src/stream"
 )
 
-func testFmapOk() {
-	slice := []int{1, 2, 3}
-	newSlice, err := stream.AsSlice[float32](stream.AsStream(slice).Fmap(func(v int) float32 { return float32(v) / 2 }))
-	if err != nil {
-		fmt.Println(err.Error())
-		panic("testFmapOk failed")
-	} else {
-		fmt.Println(len(newSlice))
-		for i := 0; i < len(newSlice); i++ {
-			fmt.Print(newSlice[i], " ")
-		}
-		fmt.Println()
-	}
+func isEven(i int) bool {
+	return i%2 == 0
 }
 
-func testFmapOkWithErr() {
-	slice := []int{1, 2, 3}
-	newSlice, err := stream.AsSlice[float32](stream.AsStream(slice).Fmap(func(v int) (float32, error) { return float32(v) / 2, nil }))
-	if err != nil {
-		fmt.Println(err.Error())
-		panic("testFmapWrongCast failed")
-	} else {
-		fmt.Println(len(newSlice))
-		for i := 0; i < len(newSlice); i++ {
-			fmt.Print(newSlice[i], " ")
-		}
-		fmt.Println()
-	}
+func square(i int) int {
+	return i * i
 }
 
-func testFmapWrongNumberOfArgs() {
-	slice := []int{1, 2, 3}
-	_, err := stream.AsSlice[float32](stream.AsStream(slice).Fmap(func(v int, x int) float32 { return float32(v+x) / 2 }))
-	if err != nil {
-		fmt.Println(err.Error())
-	} else {
-		panic("testFmapWrongNumberOfArgs failed")
-	}
-}
-
-func testFmapWrongTypeOfArg() {
-	slice := []int{1, 2, 3}
-	_, err := stream.AsSlice[float32](stream.AsStream(slice).Fmap(func(v float32) float32 { return float32(v) / 2 }))
-	if err != nil {
-		fmt.Println(err.Error())
-	} else {
-		panic("testFmapWrongTypeOfArg failed")
-	}
-}
-
-func testFmapWrongNumberOfReturnValues() {
-	slice := []int{1, 2, 3}
-	_, err := stream.AsSlice[float32](stream.AsStream(slice).Fmap(func(v int) (float32, int, int) { return float32(v) / 2, 1, 2 }))
-	if err != nil {
-		fmt.Println(err.Error())
-	} else {
-		panic("testFmapWrongNumberOfReturnValues failed")
-	}
-}
-
-func testFmapWrongReturnType() {
-	slice := []int{1, 2, 3}
-	_, err := stream.AsSlice[float32](stream.AsStream(slice).Fmap(func(v int) (float32, int) { return float32(v) / 2, 1 }))
-	if err != nil {
-		fmt.Println(err.Error())
-	} else {
-		panic("testFmapWrongReturnType failed")
-	}
-}
-
-func testFmapWrongCast() {
-	slice := []int{1, 2, 3}
-	_, err := stream.AsSlice[int](stream.AsStream(slice).Fmap(func(v int) float32 { return float32(v) / 2 }))
-	if err != nil {
-		fmt.Println(err.Error())
-	} else {
-		panic("testFmapWrongCast failed")
-	}
-}
-
-func testFmapErr() {
-	slice := []int{1, 2, 3}
-	_, err := stream.AsSlice[float32](stream.AsStream(slice).Fmap(func(v int) (float32, error) { return 0.0, fmt.Errorf("custom error") }))
-	if err != nil {
-		fmt.Println(err.Error())
-	} else {
-		panic("testFmapErr failed")
-	}
+func add(a int, b int) int {
+	return a + b
 }
 
 func main() {
-	testFmapOk()
-	testFmapOkWithErr()
-	testFmapWrongNumberOfArgs()
-	testFmapWrongTypeOfArg()
-	testFmapWrongNumberOfReturnValues()
-	testFmapWrongReturnType()
-	testFmapWrongCast()
-	testFmapErr()
+	coolNumbers := []string{
+		"42",
+		"69",
+		"13",
+		"666",
+		"1337",
+		"1",
+		"0",
+		"-5",
+		"12",
+	}
+	s := stream.AsStream(coolNumbers)
+	s = s.Fmap(strconv.Atoi).Filter(isEven).Fmap(square).Reduce(add, 0)
+	res, err := stream.AsSlice[int](s)
+	if err != nil {
+		fmt.Printf("error: %v", err.Error())
+		os.Exit(1)
+	}
+	// correct answer is 445464
+	fmt.Printf("sum of squares of even numbers: %v\n", res[0])
 }
