dd9f51e1dbdf8dc8d384b27dbc7162fe126be541
Zelourses <24527982+Zelourses@users.noreply.github.com>
2024-04-04 17:15:03 +0300

    refactor of `Take` and `TakeWhile`
    Changed:
     - `TakeWhile` now checks if the function he is using is realy a
       function
     - `TakeWhile` now first checks amount of arguments, then does the
       needed for loop
     - `Take` now uses less calls to reflection API
     - Added test for `Take`
     - fixed typo

--- src/stream/take_test.go
+++ src/stream/take_test.go
@@ -1,6 +1,7 @@
 package stream
 
 import (
+	"slices"
 	"testing"
 )
 
@@ -13,7 +14,7 @@ func TestTakeRegular(t *testing.T) {
 	if err != nil {
 		t.Errorf("expected no error, but got: %v", err.Error())
 	} else if len(newSlice) != 2 {
-		t.Errorf("expected one return value, but got: %v", len(newSlice))
+		t.Errorf("Got '%v' while expected '%v'", len(newSlice), 2)
 	}
 }
 
@@ -26,7 +27,7 @@ func TestTakeTooMuch(t *testing.T) {
 	if err != nil {
 		t.Errorf("expected no error, but got: %v", err.Error())
 	} else if len(newSlice) != 3 {
-		t.Errorf("expected one return value, but got: %v", len(newSlice))
+		t.Errorf("Got '%v' while expected '%v'", len(newSlice), 3)
 	}
 }
 
@@ -50,6 +51,24 @@ func TestTakeZero(t *testing.T) {
 	if err != nil {
 		t.Errorf("expected no error, but got: %v", err.Error())
 	} else if len(newSlice) != 0 {
-		t.Errorf("expected one return value, but got: %v", len(newSlice))
+		t.Errorf("Got '%v' while expected '%v'", newSlice, 0)
+	} else if !slices.Equal([]int{}, newSlice) {
+		t.Errorf("Got '%v' while expected '%v'", newSlice, slice)
 	}
+}
+
+func TestTakeByOne(t *testing.T) {
+	arr := []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
+	s := AsStream(arr)
+	for i := len(arr); i > 0; i-- {
+		s = s.Take(i)
+		res, err := AsSlice[int](s)
+
+		if err != nil {
+			t.Errorf("error were not expected, but got %v", err.Error())
+		} else if !slices.Equal(arr[:i], res) {
+			t.Errorf("Got '%v' while expected '%v'", res, arr[i:])
+		}
+	}
+
 }
