5e1ec77c825724b7a62d27732f9a02ec6ff1078c
unknown <axtra2@gmail.com>
2024-04-04 22:40:50 +0300

    any.go cleanup

--- stream/any.go
+++ stream/any.go
@@ -6,24 +6,26 @@ import (
 )
 
 func (s Stream) Any(f interface{}) Stream {
-	// TODO: replace with s.Valid()
-	if s.err != nil {
+	if !s.Valid() {
 		return s
 	}
+
 	fType := reflect.TypeOf(f)
+
+	if fType.Kind() != reflect.Func {
+		return s.Error(fmt.Errorf("in Any expected function, but got %v", fType))
+	}
+
 	// return type check
-	if !(fType.Out(0).Kind() == reflect.Bool && (fType.NumOut() == 1 || (fType.NumOut() == 2 && fType.Out(1).Implements(reflect.TypeOf((*error)(nil)).Elem())))) {
-		// TODO: better error message with actual types
-		return s.Error(fmt.Errorf("function used in all must return bool or (bool, error)"))
+	if !(fType.Out(0).Kind() == reflect.Bool && (fType.NumOut() == 1 || (fType.NumOut() == 2 && fType.Out(1).Implements(reflect.TypeFor[error]())))) {
+		return s.Error(fmt.Errorf("function used in Any must return bool or (bool, error)"))
 	}
 
 	// param type check
 	if fType.NumIn() != 1 {
-		// TODO: better error message with actual types
-		return s.Error(fmt.Errorf("function used in all must receive 1 argument"))
+		return s.Error(fmt.Errorf("function used in Any must receive 1 argument"))
 	}
 	if fType.In(0) != s.elemType {
-		// TODO: better error message with actual types
 		return s.Error(fmt.Errorf("type of argument 1 in function must be the same as element type of a stream"))
 	}
 
