4c1e06279452bb96e0b88a4e4081efd6c9228497
unknown <axtra2@gmail.com>
2024-04-04 22:52:48 +0300

    reduce.go cleanup

--- stream/reduce.go
+++ stream/reduce.go
@@ -11,32 +11,29 @@ func (s Stream) Reduce(f interface{}, init interface{}) Stream {
 	}
 
 	fType := reflect.TypeOf(f)
+
+	if fType.Kind() != reflect.Func {
+		return s.Error(fmt.Errorf("in Reduce expected function, but got %v", fType))
+	}
+
 	if !(fType.NumOut() == 1 || (fType.NumOut() == 2 && fType.Out(1).Implements(reflect.TypeOf((*error)(nil)).Elem()))) {
-		// TODO: better error message with actual types
-		return s.Error(fmt.Errorf("function used in reduce must return value or (value, error)"))
+		return s.Error(fmt.Errorf("function used in Reduce must return value or (value, error)"))
 	}
 
 	if fType.NumIn() != 2 {
-		// TODO: better error message with actual types
 		return s.Error(fmt.Errorf("function used in reduce must receive 2 arguments"))
 	}
 
-	// TODO: this check can be less strict
 	if fType.Out(0) != fType.In(0) {
-		// TODO: better error message with actual types
-		return s.Error(fmt.Errorf("type mismatch in reduce"))
+		return s.Error(fmt.Errorf("output of function must have the same type as its first argument"))
 	}
 
-	// TODO: this check can be less strict
 	if fType.In(0) != reflect.TypeOf(init) {
-		// TODO: better error message with actual types
-		return s.Error(fmt.Errorf("type mismatch in reduce"))
+		return s.Error(fmt.Errorf("init must have the same type as the first argument of function"))
 	}
 
-	// TODO: this check can be less strict
 	if fType.In(1) != s.elemType {
-		// TODO: better error message with actual types
-		return s.Error(fmt.Errorf("type mismatch in reduce"))
+		return s.Error(fmt.Errorf("type of argument 2 in function must be the same as element type of a stream"))
 	}
 
 	res := reflect.MakeSlice(reflect.SliceOf(reflect.TypeOf(f).Out(0)), 0, 1)
